bdcfc1d2315a7d6b3eeb4f09b1ec41dc10937ad4
hilaa <hilaa@DESKTOP-IH7ML77>
2023-05-20 23:04:05 +0300

    hila

--- my-react-app/src/pages/Albums.js
+++ my-react-app/src/pages/Albums.js
@@ -1,21 +1,26 @@
-import { useState , useEffect, useContext} from "react";
-import { Link, Outlet, useNavigate } from "react-router-dom";
-import {EssaiContext} from "./Users"
-export function Albums (){
-    const items=useContext(EssaiContext);//
-    const user = JSON.parse(localStorage.getItem('currentUser'));
-    const [current_user, setCurrentUser] = useState(user);
-    const [show_photos, setShowPhotos] = useState(false);
-    const albums_list=items.filter(album=> album.userId==current_user.id)
-    //console.log(items)
-    return(<div>{show_photos? <Outlet/>:<div>
-        <p>ALBUMS</p>
-         <ul>
-         {albums_list.map((item) =>{return <div key={item.id}><li >
-            <Link to={item.id+"/photos"} onClick={()=>{setShowPhotos(!show_photos)}}><p> {item.title}</p></Link></li>
-           </div>})}
-         </ul>
-         </div> }
-         {/* <Outlet/> */}
-    </div>)
+// Albums.js
+import { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import { EssaiContext } from "./Users";
+
+export function Albums() {
+  const items = useContext(EssaiContext);
+  const user = JSON.parse(localStorage.getItem("currentUser"));
+  const [current_user, setCurrentUser] = useState(user);
+  const albums_list = items.filter((album) => album.userId === current_user.id);
+
+  return (
+    <div>
+      <p>ALBUMS</p>
+      <ul>
+        {albums_list.map((item) => (
+          <li key={item.id}>
+            <Link to={`/users/${current_user.id}/albums/${item.id}/photos`}>
+              <p>{item.title}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
