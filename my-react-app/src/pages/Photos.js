d87d63f3e7ca5a96a84279adea7761efeef4a466
hilaa <hilaa@DESKTOP-IH7ML77>
2023-05-20 23:02:53 +0300

    hila

--- my-react-app/src/pages/Photos.js
+++ my-react-app/src/pages/Photos.js
@@ -1,47 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { useParams, useNavigate, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-export function Photos (){
-    const [photos, setPhotos] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const user = JSON.parse(localStorage.getItem('currentUser'));
-    const userid =user.id;
-    const { id } = useParams();
-    const navigate = useNavigate();
-  
-    useEffect(() => {
-      try {
-        fetch(`https://jsonplaceholder.typicode.com/photos?albumId=1`)
-          .then((response) => response.json())
-          .then((data) => {
-            setPhotos(data);
-            setIsLoading(false);
-          });
-      } catch (error) {
-        console.log(error);
-      }
-    }, [id]);
-  
-    const handleBackClick = () => {
-      navigate(`users/:${userid}/albums`);
-    };
-  
-    if (isLoading) {
-      return <h1>Loading...</h1>;
-    }
-  
-    return (
-      <div>
-        <button onClick={handleBackClick}>Back to Albums</button>
-        <div className="photo-list">
-          {photos.map((photo) => (
-            <div key={photo.id}>
-              <img src={photo.thumbnailUrl} alt={photo.title} />
-              <span>{photo.title}</span>
-            </div>
-          ))}
-        </div>
-      </div>
-    );
-  }
+export function Photos() {
+  const { albumId } = useParams();
+
+  useEffect(() => {
+    // Fetch photos based on the album ID
+    fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        // Process the fetched data
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [albumId]);
+
+  return <div>Photos Component</div>;
+}
 
